6f7a62fae41207468875647ce7635168e18ad407
joelchongg <188659314+joelchongg@users.noreply.github.com>
2025-03-22 13:36:48 +0800

    rah

--- technical-debt-analyzer/backend/server.py
+++ technical-debt-analyzer/backend/server.py
@@ -1,3 +1,4 @@
 from openai import OpenAI
 
-client = OpenAI(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)
+client = OpenAI(
+    api_key = "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")
